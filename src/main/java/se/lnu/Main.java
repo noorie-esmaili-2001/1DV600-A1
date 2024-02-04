f22d8c164d864820cc4446cb1f814f77d3a636b0
Noorie Esmaili <noorie.esmaili@gmail.com>
2024-02-04 13:37:36 +0100

    first commit

---
+++ src/main/java/se/lnu/Main.java
@@ -0,0 +1,7 @@
+package se.lnu;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/se/lnu/first Iteration/Hangman.java
@@ -0,0 +1,8 @@
+package skeleton;
+
+public class Hangman {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hangman game");
+	}
+
+}
